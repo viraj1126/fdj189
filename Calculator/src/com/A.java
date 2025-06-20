384c62c4a43b0a024871d4c75e49bbc74e058ab0
VIRAJ <virajtathe26@gmail.com>
2025-06-20 11:52:45 +0530

    add Add and sub method

---
+++ Calculator/src/com/A.java
@@ -0,0 +1,16 @@
+package com;
+
+public class A {
+	
+	
+	public void add()
+	{
+		
+		System.out.println("this is add method");
+	}
+	public void sub()
+	{
+		System.out.println("this is sub method");
+	}
+
+}
